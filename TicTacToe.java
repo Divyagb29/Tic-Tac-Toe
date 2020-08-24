a19b131637dc4a8a0c4f9e866f244d0ec9ab59a2
Divyagb29 <divya2229mgb@gmail.com>
2020-08-25 01:11:59 +0530

    choosing the letter x or o

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,7 @@
+import java.util.*;
 public class TicTacToe
 {
+	Scanner scanner=new Scanner(System.in);
 	static Random random=new Random();
 	public static void resettingBoard()
 	{
@@ -21,22 +23,30 @@ public class TicTacToe
 			}
 		}
 	}
-    public static void tossTOCheckWhoPlaysFirst()
+    public static void chooseLetter_X_or_O()
 	{
-		int toss = random.nextInt(2);
-		if(toss == 0)
+		int turn = random.nextInt(2);
+		if(turn == 0)
 		{
 			System.out.println("Player won the toss");
-		}
+			System.out.println("choose what you want to take O or X");
+			String option=scanner.next();
+			if(option.equalsIgnoreCase("x"))
+			    System.out.println("you have choose X");
+            else
+                System.out.println("you have choose O");
+        }           
 		else
 		{
 			System.out.println("Computer won the toss");
+            System.out.println("Computer choose O");
 		}
 	}
     public static void main(String[] args)
 	{
 		System.out.println("WELCOME TO TIC-TAC-TOE GAME");
 		resettingBoard();
-		tossTOCheckWhoPlaysFirst();
+		//tossTOCheckWhoPlaysFirst();
+		chooseLetter_X_or_O();
 	}
 }
