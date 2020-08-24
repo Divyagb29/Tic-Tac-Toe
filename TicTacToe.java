4fc9437184687f4ee84c0441392ba41772b6a649
Divyagb29 <divya2229mgb@gmail.com>
2020-08-24 18:32:09 +0530

    Start fresh by resetting the bord

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,7 +1,28 @@
 public class TicTacToe
 {
+	public static void resettingBoard()
+	{
+		int arr[][]=new int[3][3];
+		for (int i = 0; i < arr.length; i++)
+		{
+			for (int j = 0; j < arr.length; j++)
+			{
+				System.out.print(arr[i][j]);
+				if(j == 0 || j == 1)
+				{
+					System.out.print(" | ");
+				}
+			}	
+			System.out.println();
+			if(i == 0 || i == 1)
+			{
+				System.out.println("---------");
+			}
+		}
+	}
     public static void main(String[] args)
 	{
 		System.out.println("WELCOME TO TIC-TAC-TOE GAME");
+		resettingBoard();
 	}
 }
